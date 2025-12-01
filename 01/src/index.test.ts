54fb6aecf8b36a869e3e024da02a4fe9d16b11ce
Linus Ljung <linus.ljung@outlook.com>
2025-12-01 10:44:55 +0100

    Day 1 - Part 2

--- 01/src/index.test.ts
+++ 01/src/index.test.ts
@@ -1,6 +1,7 @@
 import getInput from '@/getInput';
 import { describe, expect, it } from 'bun:test';
 import { part1 } from 'part1';
+import { part2 } from 'part2';
 import path from 'path';
 
 const exampleInput = getInput(
@@ -15,6 +16,11 @@ describe('part1()', () => {
   });
 });
 
-describe.todo('part2()', () => {
-  it('should output the solution', () => { });
+describe('part2()', () => {
+  it('should output the solution', () => {
+    expect(part2(exampleInput)).toBe(6);
+    expect(part2(input)).toBeLessThan(7220);
+    expect(part2(input)).toBeLessThan(7076);
+    expect(part2(input)).toBe(5657);
+  });
 });
